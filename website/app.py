80b5033b4b3d28a7a7bef783c5838bf51352fba9
Juliana Zadarko <jhzadark@edu.uwaterloo.ca>
2021-09-18 20:04:23 -0400

    Uploader goes to next pg and gives file to app.py

--- website/app.py
+++ website/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask import request
 from flask import render_template
+from google.cloud import datastore
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -8,3 +10,29 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def my_form():
     print("go home")
     return render_template("home.html") # this should be the name of your html file
+
+@app.route('/store', methods=['POST'])
+def my_form_post():
+    print("hello2")
+    print(request.form)
+    af = request.form['user_af']
+    print(af)
+    print("Storing af")
+
+    # # Instantiates a client
+    # datastore_client = datastore.Client()
+    #
+    # # The kind for the new entity
+    # kind = "AudioEntries"
+    # # The name/ID for the new entity
+    # # The Cloud Datastore key for the new entity
+    # task_key = datastore_client.key(kind)
+    #
+    # # Prepares the new entity
+    # audio_entry = datastore.Entity(key=task_key)
+    # audio_entry["audio_file"] = af
+    #
+    # # Saves the entity
+    # datastore_client.put(audio_entry)
+
+    return render_template("processing.html")
